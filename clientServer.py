5ee4738aa9e63ff0ff77b98d1a2866949cbe39fd
zacharymadrazo <36104219+zacharymadrazo@users.noreply.github.com>
2018-06-24 21:50:37 -0400

    Add files via upload
    First Client, Server python script

---
+++ clientServer.py
@@ -0,0 +1,12 @@
+<?xml version="1.0"?>
+<ProfilingTarget xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <ProjectTarget>
+    <TargetProject>f0a09ea9-fd75-4a24-bb1e-b18e081e6f3a</TargetProject>
+    <FriendlyName>PythonApplication2</FriendlyName>
+  </ProjectTarget>
+  <Reports>
+    <Report>
+      <Filename>C:\Users\zacha\AppData\Local\Temp\PythonApplication21_20180624.vsp</Filename>
+    </Report>
+  </Reports>
+</ProfilingTarget>
